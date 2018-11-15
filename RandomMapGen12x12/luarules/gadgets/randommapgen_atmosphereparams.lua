f2fe295d264e8f5e9f282b5c1a76ee65aa604e5a
Fx-Doo <hedibertille93@gmail.com>
2018-11-15 16:00:15 +0100

    Renamed gadget + added atmosphere/sunParams + added waterparams gadget

--- RandomMapGen12x12/luarules/gadgets/randommapgen_atmosphereparams.lua
+++ RandomMapGen12x12/luarules/gadgets/randommapgen_atmosphereparams.lua
@@ -41,20 +41,72 @@ if gadgetHandler:IsSyncedCode() then
 	function gadget:Initialize()
 		local typemap = Spring.GetGameRulesParam("typemap")
 		local minw, maxw
+		local aparams = {}
+		local sparam = {}
 		local r = math.random
 		if typemap == "arctic" then
+			aparams = {
+			sunColor = {0.8, 0.6, 0.3, 1},
+			skyColor = {0.4, 0.4, 0.4, 0.6},
+			cloudColor = {0.6, 0.6, 0.6, 0.9},
+			fogColor = {0.4, 0.4, 0.4, 0},
+			}
+			sparams = {
+			groundAmbientColor = {0.5, 0.5, 0.5},
+			groundDiffuseColor = {0.8, 0.8, 0.8},
+			unitAmbientColor = {0.4, 0.4, 0.4},
+			unitDiffuseColor = {0.85, 0.85, 0.85},
+			}
 			minw = r(3,8)
 			maxw = r(13,20)
 		elseif typemap == "temperate" then
+			aparams = {
+			sunColor = {1, 0.8, 0.4, 1},
+			skyColor = {0.1, 0.2, 0.9, 1.0},
+			cloudColor = {0.85, 0.7, 0.7, 0.4},
+			fogColor = {0.85, 0.7, 0.7, 0.4},
+			}
+			sparams = {
+			groundAmbientColor = {0.4, 0.4, 0.4},
+			groundDiffuseColor = {0.7, 0.7, 0.7},
+			unitAmbientColor = {0.3, 0.3, 0.3},
+			unitDiffuseColor = {0.8, 0.8, 0.8},
+			}
 			minw = r(2,4)
 			maxw = r(8,13)
 		elseif typemap == "desert" then
+			aparams = {
+			sunColor = {1, 0.8, 0.4, 1},
+			skyColor = {0.1, 0.2, 0.9, 1},
+			cloudColor = {0.1, 0.2, 0.9, 0},
+			fogColor = {0.1, 0.2, 0.9, 0},
+			}
+			sparams = {
+			groundAmbientColor = {0.7, 0.7, 0.7},
+			groundDiffuseColor = {0.9, 0.9, 0.9},
+			unitAmbientColor = {0.8, 0.8, 0.8},
+			unitDiffuseColor = {1, 1, 1},
+			}
 			minw = r(1,2)
 			maxw = r(8,16)
 		else
+			aparams = {
+			sunColor = {1, 0.8, 0.8, 1},
+			skyColor = {0, 0, 0, 1},
+			cloudColor = {1, 1, 1, 1},
+			fogColor = {0, 0, 0, 1},
+			}
+			sparams = {
+			groundAmbientColor = {0.5, 0.5, 0.5},
+			groundDiffuseColor = {1, 1, 1},
+			unitAmbientColor = {0.4, 0.4, 0.4},
+			unitDiffuseColor = {1, 1, 1},
+			}
 			minw = 0
 			maxw = 1
 		end
+		Spring.SetAtmosphere(aparams)
+		Spring.SetSunLighting(sparams)
 		Spring.SetWind(minw,maxw)
 	end
 end
